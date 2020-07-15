6520776dc2baa366fda86b49802cc8b8e4a11b4e
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-07-15 22:25:57 +0100

    background image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
       <div class="p-5"></div>
-      <div class="p-5"></div>
+     
           <h1 class="text-white text-center display-2">{{ __('Welcome to GuruBoss') }}</h1>
       </div>
   </div>
