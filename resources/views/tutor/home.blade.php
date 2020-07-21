26d45929e34b57e8a9d0ea38252a63bc3511c662
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-07-21 05:29:45 +0100

    home page

--- resources/views/tutor/home.blade.php
+++ resources/views/tutor/home.blade.php
@@ -4,25 +4,7 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6">
-          <div class="card card-stats">
-            <div class="card-header card-header-warning card-header-icon">
-              <div class="card-icon">
-                <i class="material-icons">content_copy</i>
-              </div>
-              <p class="card-category">Used Space</p>
-              <h3 class="card-title">49/50
-                <small>GB</small>
-              </h3>
-            </div>
-            <div class="card-footer">
-              <div class="stats">
-                <i class="material-icons text-danger">warning</i>
-                <a href="#pablo">Get More Space...</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        
         <div class="col-lg-3 col-md-6 col-sm-6">
           <div class="card card-stats">
             <div class="card-header card-header-success card-header-icon">
@@ -30,7 +12,7 @@
                 <i class="material-icons">store</i>
               </div>
               <p class="card-category">Revenue</p>
-              <h3 class="card-title">$34,245</h3>
+              <h3 class="card-title">$0</h3>
             </div>
             <div class="card-footer">
               <div class="stats">
@@ -45,8 +27,8 @@
               <div class="card-icon">
                 <i class="material-icons">info_outline</i>
               </div>
-              <p class="card-category">Fixed Issues</p>
-              <h3 class="card-title">75</h3>
+              <p class="card-category">Classes Created</p>
+              <h3 class="card-title">0</h3>
             </div>
             <div class="card-footer">
               <div class="stats">
@@ -61,7 +43,7 @@
               <div class="card-icon">
                 <i class="fa fa-twitter"></i>
               </div>
-              <p class="card-category">Followers</p>
+              <p class="card-category">Students</p>
               <h3 class="card-title">+245</h3>
             </div>
             <div class="card-footer">
